26074cfa3ca73f68efefbdde5452d8aa6b8710a5
Muhammad Bashir Al-Kasim <bashbash390@gmail.com>
2025-12-28 12:29:30 +0100

    change smtp port

--- src/mails/mails.ts
+++ src/mails/mails.ts
@@ -4,7 +4,9 @@ import path from "path";
 
 // Create a transporter for Gmail SMTP
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
