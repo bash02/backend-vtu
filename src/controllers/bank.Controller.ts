6667958746b8f3f1e7581051c0f933e76e548526
Muhammad Bashir Al-Kasim <bashbash390@gmail.com>
2026-01-15 03:41:19 +0100

    fix user_is 3

--- src/controllers/bank.Controller.ts
+++ src/controllers/bank.Controller.ts
@@ -35,7 +35,7 @@ export const fetchProvidersController = async (req: Request, res: Response) => {
 
 export const generateDVA = async (req: Request, res: Response) => {
   try {
-    const userId = req.user?.id as string | undefined;
+    const userId = req.user._id || req.user.id || undefined;
     if (!userId)
       return res.status(400).json({ success: false, error: "User ID is required" });
     const user = await User.findById(userId);
